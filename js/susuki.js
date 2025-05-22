53a5d8164163b7327965da7372f128ebc490161f
Guilhermevssenes <gvdss14@gmail.com>
2025-05-21 21:42:59 -0300

    main

---
+++ js/susuki.js
@@ -0,0 +1,8 @@
+function mostrar(txt01)
+{
+    document.getElementById(txt01).classList.remove("d-none")
+}
+function desver(txt01)
+{
+    document.getElementById(txt01).classList.add('d-none')
+}
